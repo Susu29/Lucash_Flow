dce615a926742969ff4b5b15ee8d55daeced741a
Susu29 <50927508+Susu29@users.noreply.github.com>
2026-01-01 19:30:23 -0400

    New functions : Add/Delete supplier & customers based on the same template

--- b - TEST FOLDER/test.py
+++ b - TEST FOLDER/test.py
@@ -1,7 +1,3 @@
-<h1>HOLAAAA</h1>
-<form action = {% url 'accounting:add_accounts' %} method = "POST">
-    {% csrf_token %}
-    <label for="my_test">That's my test</label>
-    <input id="my_test" type="text" name="test_value">
+a = 123
 
-</form>
+print(a[0])
--- djangolucash/accounting/forms.py
+++ djangolucash/accounting/forms.py
@@ -3,21 +3,20 @@ from django import forms
 from django.db import models
 from accounting.models import Accounts, Suppliers, Customers
 
+from django.db.models.functions import Cast
+from django.db.models import CharField
+
 class AccountsForm(ModelForm):
     class Meta:
         model = Accounts
         fields = "__all__"
 
-class DeleteAccountsForm(ModelForm):
-    class Meta:
-        model = Accounts
-        fields = "__all__"
 
 class SelectAccountsForm(forms.Form):
     accounts = forms.ModelChoiceField(
-        queryset=Accounts.objects.all().order_by('code'),
-        label="Choose an account",
-        empty_label="Select Account"
+    queryset= Accounts.objects.order_by(Cast("code", CharField()).asc()),
+    label="Choose an account",
+    empty_label="Select Account"
     )
 
 class SuppliersForm(ModelForm):
@@ -31,14 +30,14 @@ class CustomersForm(ModelForm):
         fields = "__all__"
 
 class SelectSuppliersForm(forms.Form):
-    suppliers = forms.ModelChoiceField(
+    Suppliers = forms.ModelChoiceField(
         queryset=Suppliers.objects.all().order_by('code'),
         label="Choose a supplier",
         empty_label="Select a supplier"
     )
 
 class SelectCustomersForm(forms.Form):
-    suppliers = forms.ModelChoiceField(
+    Customers = forms.ModelChoiceField(
         queryset=Customers.objects.all().order_by('code'),
         label="Choose a customer",
         empty_label="Select a customer"
